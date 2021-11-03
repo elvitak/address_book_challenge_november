d1cb524c8a3e1f06f21a98c87610b38e33946985
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-11-03 15:09:56 +0100

    project setup create unit tests and pass them create AddressBook class, empty for now

---
+++ specs/fakeLocalStorage.js
@@ -0,0 +1,13 @@
+global.window = {};
+
+global.window.localStorage = {
+  setItem(key, value) {
+    this.data[key] = value;
+  },
+  getItem(key) {
+    return this.data[key];
+  },
+  removeItem() {},
+  clear() {},
+  data: {},
+};
